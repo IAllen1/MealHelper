0df1581f11ae4230732e687256556fb5ce916b6c
isaac <allen-i1@ulster.ac.uk>
2026-01-08 19:17:38 +0000

    Dev Branch Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    implementation("com.android.volley:volley:1.2.1")
 }
